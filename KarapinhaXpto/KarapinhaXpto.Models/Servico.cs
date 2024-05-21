c52cc0a07f8f71bc832fa32b0ca2a1935bb69fc0
Matias G Adelina <98082081+matias2k2@users.noreply.github.com>
2024-05-21 08:49:17 +0100

    primero dia de Trabalho
    vamos Familia

---
+++ KarapinhaXpto/KarapinhaXpto.Models/Servico.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarapinhaXpto.Models
+{
+    [Table("Servicos")]
+    public class Servico
+    {
+
+        [Key]
+        public int servicoid { get; set; }
+        [Required]
+        [StringLength(80)]
+        public String nomeServico { get; set; }
+        [Required]
+        [StringLength(80)]
+        public String decricao { get; set; }
+        public float valor { get; set; }
+        public Servico() { }
+    }
+}
---
+++ KarapinhaXpto/KarapinhaXpto/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KarapinhaXpto.WebApiApplication" Language="C#" %>
